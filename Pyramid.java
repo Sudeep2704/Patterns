a5b18299316bb639e1eac19f10b03a4bc0cf7d92
sudeep2704 <ds.sudeep04@gmail.com>
2025-12-06 01:00:42 +0530

    Added some more triangles

--- Pyramid.java
+++ Pyramid.java
@@ -3,10 +3,11 @@ public class Pyramid {
     public static void main(String args[]) {
 
         int n = 5;
-
+        int count = 0;
             for(int i = 0; i < n; i++) {
                             for(int j = 0; j < n-i-1; j++) {
                                     System.out.print(" ");
+                                    count++;
                             }
                                 for(int k = 0; k < 2 * i + 1; k++) {
                                         System.out.print("*");
@@ -14,7 +15,7 @@ public class Pyramid {
                                     System.out.println(" ");
             }
 
-
+            System.out.print(count);
 
     }
 
