b0111121706e5a9ab069f4dc1ba43a229574bd5b
sudeep2704 <ds.sudeep04@gmail.com>
2025-12-07 10:45:35 +0530

    Self Added codes

---
+++ HollowInvertedPyramid.java
@@ -0,0 +1,21 @@
+public class HollowInvertedPyramid {
+    
+public static void main(String args[]) {
+     int n = 5;
+    for(int i = n; i >= 1; i--) {
+        for(int j = i; j < n; j++) {
+            System.out.print(" ");
+        }
+            for(int k = 1; k <= 2 * i - 1; k++) {
+                    if(k==1|| k == 2 * i - 1||i==n) {
+                        System.out.print("*");
+                    }
+                    else {
+                        System.out.print(" ");
+                    }
+            }
+            System.out.println("");
+    }
+}
+
+}
